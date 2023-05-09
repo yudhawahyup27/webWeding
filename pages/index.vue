15f46ca709dd76db172b2d47744eb985501e8378
yudhawahyup27 <wyudha104@gmail.com>
2023-05-09 16:24:41 +0700

    modif foto background

--- pages/index.vue
+++ pages/index.vue
@@ -3,22 +3,23 @@ const config = useAppConfig()
 </script>
 <template>
     <div>
-        <div
-            class="hero  bg-[url('/assets/img/fotohero.jpg')]  bg-center saturate-50 bg-cover  bg-no-repeat h-screen bg-blend-black">
+        <div class="hero  bg-[url('/assets/img/hero.jpg')]  bg-cover   h-screen  w-screen">
             <div class="isi static">
                 <h1 class="text-center pt-10  font-bold text-xl text-white  lg:text-2xl ">Our Wedding </h1>
 
-                <h2 class="text-white font-bold absolute inset-x-24  bottom-36 px-auto py-auto text-center md:text-2xl">Yudha
+                <h2 class="text-white font-bold absolute inset-x-24  bottom-36 px-auto py-auto text-center md:text-2xl">
+                    Yudha
                     & Ima
                 </h2>
 
-                <h2 class="text-white font-bold absolute inset-x-24  bottom-28 px-auto py-auto text-center lg:text-2xl">12 . 12 .2027
+                <h2 class="text-white font-bold absolute inset-x-24  bottom-28 px-auto py-auto text-center lg:text-2xl">12 .
+                    12 .2027
                 </h2>
                 <nuxt-link to="/invitation">
-                <button
-                    class="absolute bottom-10 inset-x-5  mx-auto justify-center bg-black text-white p-4 rounded-full w-40 ">Open
-                    Invitation</button>
-                    </nuxt-link>
+                    <button
+                        class="absolute bottom-10 inset-x-5  mx-auto justify-center bg-black text-white p-4 rounded-full w-40 ">Open
+                        Invitation</button>
+                </nuxt-link>
             </div>
         </div>
     </div>
--- pages/invitation.vue
+++ pages/invitation.vue
@@ -1,8 +1,8 @@
 <template>
     <div
-        class="hero  bg-[url('/assets/img/fotohero.jpg')]  bg-center saturate-50 bg-cover  text-white  bg-no-repeat h-screen bg-blend-black">
+        class="hero   bg-[url('/assets/img/hero.jpeg')]  bg-center saturate-50 bg-cover  bg-fixed text-white h-screen w-screen ">
         <h1 class="text-xl  font-bold text-center pt-10">We Are Getting Marrid</h1>
         <p class="text-lg  text-center pt-2">Yudha & Ima</p>
-        
+
     </div>
 </template>
