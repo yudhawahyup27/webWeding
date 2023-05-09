a47d1d93784c028bb819e468b812792b45c7b6af
yudhawahyup27 <wyudha104@gmail.com>
2023-05-09 15:44:52 +0700

    first commit

---
+++ pages/invitation.vue
@@ -0,0 +1,8 @@
+<template>
+    <div
+        class="hero  bg-[url('/assets/img/fotohero.jpg')]  bg-center saturate-50 bg-cover  text-white  bg-no-repeat h-screen bg-blend-black">
+        <h1 class="text-xl  font-bold text-center pt-10">We Are Getting Marrid</h1>
+        <p class="text-lg  text-center pt-2">Yudha & Ima</p>
+        
+    </div>
+</template>
